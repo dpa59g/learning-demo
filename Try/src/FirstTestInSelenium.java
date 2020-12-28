3720ac9a8fcb726c1e744722ee454df9a1253a90
dpa <whynotdeepa@gmail.com>
2020-12-28 22:33:12 +0530

    First commit Simple code to enter google url in firefox

---
+++ Try/src/FirstTestInSelenium.java
@@ -0,0 +1,32 @@
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class FirstTestInSelenium {
+
+public static void main(String[] args) {
+// TODO Auto-generated method stub
+
+//setting the driver executable
+System.setProperty("webdriver.gecko.driver", "D:\\Deepa\\Selenium Workspace\\Selenium Installations\\geckodriver.exe");
+
+//Initiating your chromedriver
+WebDriver driver=new FirefoxDriver();
+
+//Applied wait time
+driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+//maximize window
+driver.manage().window().maximize();
+
+//open browser with desried URL
+driver.get("https://www.google.com");
+
+//closing the browser
+driver.close();
+
+}
+
+}
